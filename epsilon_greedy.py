18d9321c68ec1efc1d60b15002e310f6bb694d30
Aayushi Gupta <aayushiguptaeconomist@gmail.com>
2026-02-01 14:57:31 +0530

    first commit

---
+++ epsilon_greedy.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+NUM_TRIALS = 10000
+EPS = 0.1   # probability of exploration (random) => 0.9 is the probability of exploitation (highest win rate)
+BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]  # true success probabilities for each bandit (here bandit equals to a slot machine with a lever)
+
+class Bandit:
+    def __init__ (self, p):
+        self.p = p  # true probability of winning
+        self.p_estimate = 0 # estimated probability of winning
+        self.N = 0  # number of times a bandit is pulled
+
+    def pull(self):
+        return np.random.random() < self.p  # np.random.random() generates a random no. b/w 0 and 1. Then, it returns True(1) if < p else False(0).
+    
+    def update(self, x):
+        self.N += 1
+        self.p_estimate = self.p_estimate + ((x - self.p_estimate) / self.N)    # this is basically calculating the average win rate
+
+def experiment():
+    bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
+
+    # initialize a few variables
+    rewards = np.zeros(NUM_TRIALS)
+    num_times_explored = 0
+    num_times_exploited = 0
+    num_optimal = 0 # number of times the algorithm chose the best bandit
+    optimal_j = np.argmax([b.p for b in bandits])   # index of the best bandit
+    print("optimal j:", optimal_j)
+
+    for i in range(NUM_TRIALS):
+        # use epsilon-greedy to select the bandit
+
+        # if else condition is just to decide whether to explore or exploit and choose a bandit
+        if np.random.random() < EPS:
+            num_times_explored += 1
+            j = np.random.randint(len(bandits)) # select a random bandit by generating a random no. b/w 0 and len(bandits)-1
+
+        else:
+            num_times_exploited += 1
+            j = np.argmax([b.p_estimate for b in bandits])
+            print(f"for index {i}: {[b.p_estimate for b in bandits]}")
+
+        # if the selected bandit is the optimal bandit, increment num_optimal
+        if j == optimal_j:
+            num_optimal += 1
+
+        # pull the arm for the bandit with the largest sample mean
+        x = bandits[j].pull()
+
+        # update rewards log
+        rewards[i] = x
+
+        bandits[j].update(x)
+
+    # print the mean estimates for each bandit
+    for b in bandits:
+        print("mean estimate: ", b.p_estimate)
+
+    print("Total reward earned: ", rewards.sum())
+    print("Overall win rate: ", rewards.sum() / NUM_TRIALS)
+    print("Number of times explored: ", num_times_explored)
+    print("Number of times exploited: ", num_times_exploited)
+    print("Number of times selected optimal bandit: ", num_optimal)
+
+    # plot the results
+    cumulative_rewards = np.cumsum(rewards)
+    win_rates = np.cumsum(rewards) / (np.arange(NUM_TRIALS) + 1)    # adding 1 to an array basically add to each element of the array which avoids division by 0 as the first index
+    print("cumulative win rates: ", win_rates)
+    plt.plot(win_rates)
+    plt.plot(np.ones(NUM_TRIALS) * np.max(BANDIT_PROBABILITIES))
+    plt.savefig("win_rates.png")
+
+if __name__ == "__main__":
+    experiment()
+
+
+
---
+++ optimistic_initial_value.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from epsilon_greedy import experiment
+
+# from epsilon_greedy import experiment
+
+NUM_TRIALS = 10000
+EPS = 0.1
+BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
+
+class Bandit:
+    def __init__ (self, p):
+        self.p = p
+        self.p_estimate = 5    # optimistic initial value is high
+        self.N = 1  # if we do not initialize to 1, then the first update will erase any effect of the optimistic initial value 
+
+    def pull(self):
+        return np.random.random() < self.p  # generates rewards
+        
+    def update(self, x):
+        self.N += 1
+        self.p_estimate = self.p_estimate + ((x - self.p_estimate) / self.N)
+
+def experiment():
+        bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
+
+        rewards = np.zeros(NUM_TRIALS)
+        # num_times_explored = 0
+        # num_times_exploited = 0
+        # num_optimal = 0
+        # optimal_j = np.argmax(b.p for b in bandits)
+        # print("optimal j:", optimal_j)
+
+        for i in range(NUM_TRIALS):
+            j = np.argmax([b.p_estimate for b in bandits])
+            # print(f"for index {i} and chosen bandit {j} : {[b.p_estimate for b in bandits]}")
+
+            x = bandits[j].pull()
+            # print(f"reward received: {x}")
+
+            rewards[i] = x
+
+            bandits[j].update(x)
+
+        for b in bandits:
+            print("mean estimate: ", b.p_estimate)
+
+        print("total rewards earned: ", rewards.sum())
+        print("overall rewards earned: ", rewards.sum()/ NUM_TRIALS)
+        print("num of times selected each bandit: ", [b.N for b in bandits])
+
+        cumulative_rewards = np.cumsum(rewards)
+        win_rates = cumulative_rewards / np.arange(1, NUM_TRIALS + 1)
+        plt.ylim([0, 1])    # sets y-axis range from 0 to 1
+        plt.plot(win_rates)
+        # plt.plot(np.ones(NUM_TRIALS) * 5) # plots a horizontal line at the optimistic initial value
+        plt.plot(np.ones(NUM_TRIALS) * np.max(BANDIT_PROBABILITIES))  # plots a horizontal line at the optimal win rate
+        plt.legend(['Win Rate', 'Optimal Bandit'])
+        plt.show()
+
+
+if __name__ == "__main__":
+        experiment()
+
+    
